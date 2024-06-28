38c72bfe5370e3588f8e6c5658f4c1e77cd69c2c
Faizan Moriani <46195130+Faizanmoriani2@users.noreply.github.com>
2024-06-28 12:40:22 +0500

    trying to connect backend :( and added some functionalities

---
+++ app/src/main/java/com/example/shopinventorymanagement/utils/Constant.kt
@@ -0,0 +1,17 @@
+package com.example.shopinventorymanagement.utils
+
+
+object Constant {
+    const val PREF_NAME = "pref_shop_app"
+
+//    const val REGISTER_USER = "REGISTER_USER"
+    const val ADD_PRODUCT = "ADD_PRODUCT"
+    const val GET_PRODUCTS = "GET_PRODUCTS"
+    const val UPDATE_PRODUCT = "UPDATE_PRODUCT"
+    const val DELETE_PRODUCT = "DELETE_PRODUCT"
+
+//    const val KEY_USER_ID = "user_id"
+//    const val KEY_USER_NAME = "user_name"
+    const val KEY_REASON= "reason"
+
+}
